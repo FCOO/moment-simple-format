8d3179a5d952e0e0e06c81e86f7be80c8fdb1b59
NielsHolt <niels@steenbuchholt.dk>
2016-09-28 22:27:11 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # moment-simple-format
+>
+
+
+## Description
 Plugin to Moment with easy formatting and timezone selection
+
+## Installation
+### bower
+`bower install https://github.com/FCOO/moment-simple-format.git --save`
+
+## Demo
+http://FCOO.github.io/moment-simple-format/demo/ 
+
+## Usage
+
+Simplifies the format of moment by setting
+
+1. The format for time is `'24'` or '`12'` ('14:00' or '02:00 pm')
+2. Witch time-zone the moment is displayed in:
+    - local
+    - utc
+    - or any `moment.tz` time-zone added by `moment.sfAddTimezone`
+3. The sequence of the different parts:
+    - `DMY`: Day-Month-Year
+    - `MDY`: Month-Day-Year
+    - `YMD`: Teay-Month-Day
+4. A date-string is formed by setting one of tree formats for each of the following tree parts: 
+    - `weekday`: Format = `'Full'`, `'Short'`, or `'None'` (alt. `'F'`/`'S'`/`'N'`)
+    - `month  `: Format = `'Full'`, `'Short'`, or `'Digital'` (alt. `'F'`/`'S'`/`'D'`) 
+    - `year   `: Format = `'Full'`, `'Short'`, or `'None'`  (alt. `'F'`/`'S'`/`'N'`)
+5. A relative moment-string is in the form `[now](+|-)[d[day-abbr]][h[hour-abbr]][mm[minutes-abbr]]`. Witch part to include is set by options `relativeFormat: {now: true, days: false, hours: true, minutes: true}` and options `text: {utc: 'UTC', local: 'local', dayAbbr : 'd', hourAbbr: 'h', minAbbr : 'm', now: 'now', to: 'to'}``
+
+### options
+The full options for setting the format:
+
+	{
+		time          : 24,       // = '24'/24/'12'/12
+		timezone      : 'local',  // = 'local' or 'utc' or the id of a timezone added with moment.sfAddTimezone
+		date          : 'DMY',    // = 'DMY'/'MDY'/'YMD'
+		dateFormat    : {
+			weekday: 'None',  // = 'full'/'f'/'short'/'s'/'none'/'n'
+			month  : 'Short', // = 'full'/'f'/'short'/'s'/'digital'/'d'
+			year   : 'Full'   // = 'full'/'f'/'short'/'s'/'none'/'n'
+		},
+		relativeFormat: {
+			now    : true, 
+			days   : false, 
+			hours  : true,
+			minutes: true
+		},
+		text: {
+			utc     : 'UTC',
+			local   : 'local',
+			dayAbbr : 'd',
+			hourAbbr: 'h',
+			minAbbr : 'm',
+			now     : 'now',
+			to      : 'to'
+		}
+	}
+
+
+There is a special case: `dateFormat: {weekday:'none', month:'none', year:'none'}` => only the date of the month
+
+#### Examples of `dateFormat` (with `date:'DMY'`)
+`dateFormat: {weekday:'F', month:'F', year:'F'}` => format = `'dddd, DD. MMMM YYYY'` (eg. `'Monday, 24. December 2015'`)
+`dateFormat: {weekday:'N', month:'S', year:'F'}` => format = `'DD. MMM YYYY'`  (eg. `'24. Dec 2015'`)
+`dateFormat: {weekday:'N', month:'D', year:'S'}` => format = `'DD/MM/YY'`  (eg. `'24/12/15'`)
+
+### Methods
+
+##### `moment.sfInit( options )`
+Initialize the options and the list of time-zones
+Only need to be call if `options.text` is changed
+
+##### `moment.sfSetFormat( options )`
+Set the `options`
+
+##### `moment.sfAddTimezone( options )`
+Adds a time-zone to the list of available time-zones
+
+    options = {id: [String], //id of the timezone from moment.tz
+               name: [String] //optional name for the timezone
+              }
+All the timezones are in `moment.simpleFormat.timezoneList //[]`
+
+##### `moment.sfDateFormatList( includeCodeFunc )`
+Return a array of available formats. 
+
+`includeCodeFunc = function( code )`: optional - return true or false to include or exclude a format with code from the list
+
+
+##### `moment().tzMoment( [timezone] )`
+Return the moment adjusted to `timezone` or the time-zone set with `moment.sfSetFormat`
+
+##### `moment().dateFormat( [options] )`
+Return a formatted date string. The format is given by `options` or the options set with `moment.sfSetFormat`
+
+##### `moment().timeFormat( [options] )`
+Return a formatted time string. The format is given by `options` or the options set with `moment.sfSetFormat`
+
+##### `moment().dateTimeFormat( [options] )`
+Return a formatted date and time string. The format is given by `options` or the options set with `moment.sfSetFormat`
+
+##### `moment().relativeFormat( [options] )`
+Return a relative time string. The format is given by `options` or the options set with `moment.sfSetFormat`
+
+
+
+## Copyright and License
+This plugin is licensed under the [MIT license](https://github.com/FCOO/moment-simple-format/LICENSE).
+
+Copyright (c) 2016 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+Niels Holt nho@fcoo.dk
---
+++ src/moment-simple-format.js
@@ -0,0 +1,245 @@
+/****************************************************************************
+	moment-simple-format.js, 
+
+	(c) 2016, FCOO
+
+	https://github.com/FCOO/moment-format
+	https://github.com/FCOO
+
+****************************************************************************/
+
+(function (moment, $ /*, window, document, undefined*/) {
+	"use strict";
+ 
+    /***********************************************************
+    dateFormatList = array[0..dateFormats-1] of {DMY: [String], MDY: [String], YMD: [String]}
+    List of different format-strings in tree different sequence
+    ***********************************************************/
+    var dateFormatList = [],
+        w, m, y, 
+        wFormat, mFormat, backslash, yFormat, dateFormat;
+    for (w in {F:'', S:'', N:''})
+        for (m in {F:'', S:'', D:''})
+            for (y in {F:'', S:'', N:''}){
+                dateFormat = {code: w+m+y, DMY: '', MDY: '', YMD:''};
+                wFormat = w == 'F' ? 'dddd, ' : w == 'S' ? 'ddd, ' : '';
+                mFormat = m == 'F' ? 'MMMM'   : m == 'S' ? 'MMM'   : 'MM';
+                yFormat = y == 'F' ? 'YYYY'   : y == 'S' ? 'YY'    : '';
+                backslash = mFormat == 'MM'; 
+                
+                dateFormat.DMY = wFormat + 'DD' + (backslash ? '/' : '. ') + mFormat + (yFormat ? (backslash ? '/' : ' ') + yFormat : '');
+                dateFormat.MDY = wFormat + mFormat + (backslash ? '/' : ' ') + 'DD' + (yFormat ? (backslash ? '/' : ', ') + yFormat : '');
+                dateFormat.YMD = wFormat + (yFormat ? yFormat + (backslash ? '/' : ' ') : '') + mFormat + (backslash ? '/' : ' ') + 'DD';
+
+                dateFormatList.push( dateFormat );
+            }
+    //Special case: NNN
+    dateFormatList.push( { code:'NNN', DMY: 'DD', MDY: 'DD', YMD: 'DD'} );
+    
+    /* 'Handmade' version to see the principle 
+        { code:'FFF', DMY: 'dddd, DD. MMMM YYYY', MDY: 'dddd, MMMM DD, YYYY', YMD: 'dddd, YYYY MMMM DD' },    //Monday, 24. December 2015 | Monday, December 24, 2015 | Monday, 2015 December 24
+        { code:'SFF', DMY: 'ddd, DD. MMMM YYYY' , MDY: 'ddd, MMMM DD, YYYY' , YMD: 'ddd, YYYY MMMM DD'  },    //Mon, 24. December 2015    | Monday, December 24, 2015 | Mon, 2015 December 24
+        { code:'SSF', DMY: 'ddd, DD. MMM YYYY'  , MDY: 'ddd, MMM DD, YYYY'  , YMD: 'ddd, YYYY MMM DD'   },    //Mon, 24. Dec 2015         | Mon Dec 24, 2015          | Mon 2015 Dec 24
+        { code:'SSS', DMY: 'ddd, DD. MMM YY'    , MDY: 'ddd, MMM DD, YY'    , YMD: 'ddd, YY MMM DD'     },    //Mon, 24. Dec 15           | Mon Dec 24, 15            | Mon 15 Dec 24
+        { code:'NSF', DMY: 'DD. MMM YYYY'       , MDY: 'MMM DD, YYYY'       , YMD: 'YYYY MMM DD'        },    //24. Dec 2015              | Dec 24, 2015              | 2015 Dec 24
+        { code:'NSS', DMY: 'DD. MMM YY'         , MDY: 'MMM DD, YY'         , YMD: 'YY MMM DD'          },    //24. Dec 15                | Dec 24, 15                | 15 Dec 24
+        { code:'NDF', DMY: 'DD/MM/YYYY'         , MDY: 'MM/DD/YYYY'         , YMD: 'YYYY/MM/DD'         },    //24/12/2015                | 12/24/2015                | 2015/12/24
+        { code:'NDS', DMY: 'DD/MM/YY'           , MDY: 'MM/DD/YY'           , YMD: 'YY/MM/DD'           },    //24/12/15                  | 12/24/15                  | 15/12/24
+        { code:'NDN', DMY: 'DD/MM'              , MDY: 'MM/DD'              , YMD: 'MM/DD'              },    //24/12                     | 12/24                     | 12/24
+
+        { code:'NNN', DMY: 'DD'                 , MDY: 'DD'                 , YMD: 'DD'                 },    //24                        | 24                        | 24
+    */
+
+    var defaultOptions = {
+            time          : 24,
+            timezone      : 'local',
+            date          : 'DMY',
+            dateFormat    : {
+                weekday: 'None', 
+                month  : 'Short', 
+                year   : 'Full'
+            },
+            relativeFormat: {
+                now    : true, 
+                days   : false, 
+                hours  : true,
+                minutes: true
+            },
+            text: {
+                utc     : 'UTC',
+                local   : 'local',
+                dayAbbr : 'd',
+                hourAbbr: 'h',
+                minAbbr : 'm',
+                now     : 'now',
+                to      : 'to'
+            }
+        },
+
+        //Create namespace
+        namespace = moment.simpleFormat = {
+            options       : defaultOptions,
+            code          : '',
+            dateFormat    : '',
+            timeFormat    : '',
+            timezoneList  : [],
+            relativeFormat: ''
+        };
+
+    
+
+    //Global const, var, and methods
+
+
+    // moment.sfSetFormat
+    moment.sfSetFormat = function( options ){ 
+        function convert( singleOption ){
+            singleOption = singleOption.toUpperCase();
+            return singleOption == 'FULL'    ? 'F' :
+                   singleOption == 'SHORT'   ? 'S' : 
+                   singleOption == 'DIGITAL' ? 'D' : 
+                   singleOption == 'NONE'    ? 'N' : 
+                                   singleOption;
+        } 
+        $.extend( true, namespace.options, options );
+
+        var i,
+            opt_dateFormat = namespace.options.dateFormat,
+            opt_relativeFormat = namespace.options.relativeFormat,
+            opt_text = namespace.options.text;
+
+        namespace.code = convert( opt_dateFormat.weekday ) + convert( opt_dateFormat.month ) + convert( opt_dateFormat.year );
+            
+        for (i=0; i<dateFormatList.length; i++ )
+            if (dateFormatList[i].code == namespace.code){
+              namespace.dateFormat = dateFormatList[i][ namespace.options.date ]; 
+              break;
+            }
+
+        namespace.timeFormat = parseInt(namespace.options.time) == 24 ? 'HH:mm' : 'hh:mma';
+        
+        for (i=0; i<namespace.timezoneList.length; i++ )
+            if (namespace.timezoneList[i].id == namespace.options.timezone){
+                namespace.timezone = namespace.timezoneList[i];
+                break;
+            }
+
+        namespace.relativeFormat = (opt_relativeFormat.days    ? 'd['  + opt_text.dayAbbr  + ']' : '') + 
+                                   (opt_relativeFormat.hours   ? 'h['  + opt_text.hourAbbr + ']' : '') +
+                                   (opt_relativeFormat.minutes ? 'mm[' + opt_text.minAbbr  + ']' : '');
+    
+    };
+    
+    // moment.sfAddTimezone
+    moment.sfAddTimezone = function( options ){ 
+        var THIS = this;
+        if ($.isArray( options ))
+            $.each( options, function( index, opt ){ THIS.sfAddTimezone( opt ); } );
+        else { 
+            options.name = options.name || options.id;
+            var offset = 0;
+            switch (options.id){
+                case 'local': offset = (new Date()).getTimezoneOffset();    break;
+                case 'utc'  : offset = null; break;
+                default     : offset = window.moment.tz.zone(options.id).parse(Date.UTC()); break;
+            }
+            options.fullName = options.name;                      
+            if (offset !== null){
+                options.fullName += ' (UTC' + (offset<0?'+':'-');
+                offset = Math.abs(offset);        
+                var h = Math.floor(offset / 60),
+                    m = offset % 60;
+                options.fullName += (h<10?'0':'') + h + ':' + (m<10?'0':'') + m + ')';
+            }
+            namespace.timezoneList.push(options);
+        }
+    };
+    
+    // moment.sfDateFormatList
+    moment.sfDateFormatList = function( includeCodeFunc ){
+        includeCodeFunc = includeCodeFunc || function(/* code */){ return true; };
+        var i, dateFormat, result = [];
+        for (i=0; i<dateFormatList.length; i++ ){
+            dateFormat = dateFormatList[i];
+            if (includeCodeFunc( dateFormat.code ) ){
+                result.push( dateFormat[ namespace.options.date ] );              
+            }
+        }
+        return result;
+    };
+
+    // moment.sfInit
+    moment.sfInit = function( options ){
+        this.sfSetFormat( options );
+
+        this.timezoneList = [];
+        this.sfAddTimezone([
+            { id:'local', name: namespace.options.text.local },
+            { id:'utc',   name: namespace.options.text.utc   }
+        ]);
+    };
+
+    moment.sfInit();
+    
+
+    //Moment.prototype.method == moment.fn.method
+
+    // moment.fn.tzMoment
+    moment.fn.tzMoment = function( timezone ) { 
+        timezone = timezone || namespace.options.timezone;
+        if (timezone == 'local') return this.local();
+        if (timezone == 'utc') return this.utc();
+        return this.tz( timezone );
+    };
+
+
+    //moment.fn._sfAnyFormat
+    moment.fn._sfAnyFormat = function( options, func ){
+        if (options){
+            var saveOptions = $.extend(true, {}, namespace.options);
+            moment.sfSetFormat( options );
+        }
+
+        var result = $.proxy( func, this )();
+
+        if (options)
+            moment.sfSetFormat( saveOptions );
+
+        return result;
+    };
+
+
+
+    // moment.fn.dateFormat
+    moment.fn.dateFormat = function( options ) { 
+        return this._sfAnyFormat( options, function(){
+            return this.format( namespace.dateFormat );
+        });
+    };
+
+    // moment.fn.timeFormat
+    moment.fn.timeFormat = function( options ) { 
+        return this._sfAnyFormat( options, function(){
+            return this.format( namespace.timeFormat );
+        });
+    };
+
+    // moment.fn.dateTimeFormat
+    moment.fn.dateTimeFormat = function( options ) { 
+        return this.dateFormat( options ) + ' ' + this.timeFormat( options );
+    };
+
+    // moment.fn.relativeFormat
+    moment.fn.relativeFormat = function( options ) { 
+        return this._sfAnyFormat( options, function(){
+            var minDiff = this.diff( moment() , '', true),
+                sign = minDiff < 0 ? '-' : '+';
+            minDiff = Math.abs( minDiff ) ;
+            return (namespace.options.relativeFormat.now ? namespace.options.text.now : '') + sign + moment.duration(minDiff).format( namespace.relativeFormat );
+        });
+    };
+
+
+
+
+}(moment, jQuery, this, document));
